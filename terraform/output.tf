53cb3e17cbe634a5468db2329cbfa37fd0f13c2a
Atharv Vadehra <atharv@vadehra.me>
2026-09-25 21:45:36 +0530

    Iniciated a multi repo style.

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "vm_public_ip" {
-  value       = azurerm_public_ip.pip.ip_address
-  description = "Public IP address of the honeypot sensor VM"
-}
-
-output "monitoring_vm_public_ip" {
-  value       = azurerm_public_ip.pip_monitoring.ip_address
-  description = "Public IP address of the SecDash monitoring & operations VM"
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "admin_public_ip" {
-  type        = string
-  description = "Allowed administrative public IP address for SSH and management access"
-
-  validation {
-    condition     = can(regex("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$", var.admin_public_ip))
-    error_message = "The admin_public_ip must be a valid IPv4 address without CIDR (e.g. 203.0.113.45)."
-  }
-}
